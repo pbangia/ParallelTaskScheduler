136ab4924bdbd4f7d107083d1357bebd2bd372ed
Taranpreet Kohli <tkoh638@aucklanduni.ac.nz>
2016-07-20 14:16:54 +1200

    Maven project setup.

---
+++ src/test/java/project/MainTest.java
@@ -0,0 +1,14 @@
+package project;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void SampleTest_AssertsTrue(){
+        assertEquals(true, 5 < 10);
+    }
+
+}
