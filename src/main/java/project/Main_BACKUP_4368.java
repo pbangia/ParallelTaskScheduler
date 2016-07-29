0adaa784058f41129b49145b940cc35d61eda8f8
Priyankit Bangia <priyankit.bangia@gmail.com>
2016-07-29 15:51:08 +1200

    test2

---
+++ src/main/java/project/Main_BACKUP_4368.java
@@ -0,0 +1,15 @@
+package project;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        // Store arguments
+        // Read file
+        // Transform file to Node object (for now)
+        // Proceed with rest of program
+
+    }
+
+
+}
