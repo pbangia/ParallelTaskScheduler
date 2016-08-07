bf0a5019b09320254344cd9ca288fe603bca54b5
Taranpreet Kohli <tkoh638@aucklanduni.ac.nz>
2016-08-07 15:42:04 +1200

    Fixed JavaDoc for find roots method

--- src/main/java/app/schedule/SchedulerHelper.java
+++ src/main/java/app/schedule/SchedulerHelper.java
@@ -11,23 +11,27 @@ public class SchedulerHelper {
 
     private static Logger logger = LoggerFactory.getLogger(SchedulerHelper.class);
 
+    /**
+     * Finds the roots of a provided digraph represented in a data map.
+     * Algorithm:
+     * 1. All all of the Nodes to a set of Nodes.
+     * 2. Iterate through each Node in that set
+     * 3. For each child that the current Node has, remove the child Node from the set of Nodes.
+     * 4. Any nodes leftover in the set are your Nodes.
+     * @param dataMap representing the digraph
+     * @return List of roots in the digraph
+     * @throws NoRootFoundException if no roots are found in the digraph (for e.g. cyclic graphs)
+     */
     public List<Node> findRoots(Map<String, Node> dataMap) throws NoRootFoundException {
 
         Set<Node> nodeSet = new HashSet<>();
 
-        /**
-         * Add all of the Nodes to the set of nodes.
-         */
         Iterator<Map.Entry<String, Node>> entryIterator = dataMap.entrySet().iterator();
         while (entryIterator.hasNext()){
             Map.Entry<String, Node> currentEntry = entryIterator.next();
             nodeSet.add(currentEntry.getValue());
         }
 
-        /**
-         * Iterate through all of the Nodes, and for each child
-         * in the current Node, remove the child Node from the set of Nodes.
-         */
         entryIterator = dataMap.entrySet().iterator();
         while (entryIterator.hasNext()){
             Node currentNode = entryIterator.next().getValue();
