c3c078c213637fc07a5ce3f7142178f5dd54bbe8
hailun wang <hailun wang>
2016-08-07 13:00:44 +1200

    Added call from Scheduler to getAvailableNodes method

--- src/main/java/app/schedule/TaskScheduler.java
+++ src/main/java/app/schedule/TaskScheduler.java
@@ -2,6 +2,7 @@ package app.schedule;
 
 
 import app.data.Node;
+import app.utils.MapUtils;
 
 import java.util.*;
 
@@ -9,9 +10,17 @@ public class TaskScheduler {
 
     private Set<Node> scheduledNodes = new HashSet<>();
     private Map<String, Node> dataMap;
+    private MapUtils mapUtils;
+    private List<Node> nextAvailableNodes;
 
-    public TaskScheduler(Map<String, Node> dataMap){
+    public TaskScheduler(Map<String, Node> dataMap, MapUtils mapUtils){
         this.dataMap = dataMap;
+        this.mapUtils = mapUtils;
+    }
+
+    //void for now as not sure what it will be returning.
+    public void scheduleTasks(){
+        nextAvailableNodes = mapUtils.getAvailableNodes(null, dataMap, scheduledNodes);
     }
 
     public void setScheduledNodes(Node node){
