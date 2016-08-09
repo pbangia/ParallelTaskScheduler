85491086daecd023667102f729295871cbd417a7
Taranpreet Kohli <tkoh638@aucklanduni.ac.nz>
2016-08-09 22:22:51 +1200

    Created a TaskSchedulerFactory class, as well as cleanup of the main class that runs all of the modules.

---
+++ src/main/java/app/schedule/TaskSchedulerFactory.java
@@ -0,0 +1,13 @@
+package app.schedule;
+
+import app.data.Node;
+
+import java.util.Map;
+
+public class TaskSchedulerFactory {
+
+    public static TaskScheduler createTaskScheduler(Map<String, Node> dataMap, int numProcessors){
+        SchedulerHelper schedulerHelper = new SchedulerHelper();
+        return new TaskScheduler(dataMap, schedulerHelper, numProcessors);
+    }
+}
