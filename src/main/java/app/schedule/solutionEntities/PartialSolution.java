da2b79e69e903fdb2bc4ea566c5f3ca67a023b15
gtiongco95 <g.tiongco95@gmail.com>
2016-08-09 22:49:08 +1200

    Added missing imports

--- src/main/java/app/schedule/solutionEntities/PartialSolution.java
+++ src/main/java/app/schedule/solutionEntities/PartialSolution.java
@@ -1,8 +1,12 @@
 package app.schedule.solutionEntities;
 
 import app.data.Node;
-
-import java.util.*;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Iterator;
 
 public class PartialSolution {
 
