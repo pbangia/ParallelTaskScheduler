1930033e8d4c9093ee6e09ad8c9a27019cb44cfa
Priyankit Bangia <priyankit.bangia@gmail.com>
2016-08-09 20:40:57 +1200

    Implemented clone test in PartialSolutionTest.

--- src/test/java/app/schedule/solutionEntities/PartialSolutionTest.java
+++ src/test/java/app/schedule/solutionEntities/PartialSolutionTest.java
@@ -1,7 +1,16 @@
 package app.schedule.solutionEntities;
 
+import app.data.Node;
+import org.apache.commons.collections4.CollectionUtils;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import static org.junit.Assert.*;
 
@@ -28,6 +37,47 @@ public class PartialSolutionTest {
 
     @Test
     public void testClone() throws Exception {
+        Node n1a = new Node("A",1);
+        Node n1b = new Node("B",1);
+        Queue<Node> nq1 = new ConcurrentLinkedQueue<Node>();
+        nq1.add(n1a);
+        nq1.add(n1b);
+        Map<Integer, Integer> ts1=new ConcurrentHashMap<Integer, Integer>();
+        ts1.put(1,1);
+        ts1.put(2,2);
+        Map<Node, Integer> ets1=new ConcurrentHashMap<Node, Integer>();
+        ets1.put(n1a,1);
+        ets1.put(n1b,1);
+        Processor p1 = new Processor();
+
+        p1.setQueueSize(1);
+        p1.setCurrentTimeStamp(1);
+        p1.setTimeStampMap(ts1);
+        p1.setNodeQueue(nq1);
+        p1.setNodeEndTimeMap(ets1);
+
+        Processor p2 = p1.clone();
+
+        assertNotEquals(p1,p2);
+
+        assertEquals(p1.getCurrentTimeStamp(),p2.getCurrentTimeStamp());
+
+        assertEquals(p1.getQueueSize(),p2.getQueueSize());
+
+        assertTrue(CollectionUtils.isEqualCollection(p1.getNodeQueue(),p2.getNodeQueue()));
+        assertNotEquals(p1.getNodeQueue(),p2.getNodeQueue());
+
+        //Keys+Values are equal collections. Keys (nodes) are the same objects, values are not.
+        assertTrue(CollectionUtils.isEqualCollection(p1.getNodeEndTimeMap().values(),p2.getNodeEndTimeMap().values()));
+        assertNotEquals(p1.getNodeEndTimeMap().values(),p2.getNodeEndTimeMap().values());
+        assertTrue(CollectionUtils.isEqualCollection(p1.getNodeEndTimeMap().keySet(),p2.getNodeEndTimeMap().keySet()));
+        assertEquals(p1.getNodeEndTimeMap().keySet(),p2.getNodeEndTimeMap().keySet());
+        assertEquals(p1.getNodeEndTimeMap(),p2.getNodeEndTimeMap());
+
+        //unsure. assertEquals for maps compares contents of maps according to docs, not reference.
+        assertEquals(p1.getTimeStampMap(),p2.getTimeStampMap());
+        assertNotEquals(p1.getTimeStampMap().values(),p2.getTimeStampMap().values());
+
 
     }
 
