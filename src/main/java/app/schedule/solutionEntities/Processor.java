6e8b5e4614c0a1a9b2c62dd7b91e194e9e7e1570
hailun wang <hailun wang>
2016-08-09 14:31:26 +1200

    Implemented data structures to hold nodes in processor class. Implemented addNodes method as well as getters.

--- src/main/java/app/schedule/solutionEntities/Processor.java
+++ src/main/java/app/schedule/solutionEntities/Processor.java
@@ -1,9 +1,45 @@
 package app.schedule.solutionEntities;
 
+import app.data.Node;
+
+import java.util.Map;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
 public class Processor {
-    //needs constructor
-    //needs add to processors method
     //needs data strucutre to hold the nodes and the start and end times
-    //needs a get method which returns the data structure
+
+    private Queue<Node> nodeQueue = new ConcurrentLinkedQueue<>(); // DATA STRUCTURE can be a queue, linkedlist or arraylist... should probably implement the List interface
+    private Map<Integer, Integer> timeStampMap = new ConcurrentHashMap<>();
+    private int queueSize = 0;
+    private int currentTimeStamp = 0;
+
+
+    public void addNodeToQueue(Node node, int waitTime){
+        nodeQueue.add(node);
+        //adding wait time to the when the last node ended. This calculates new timestamp for currrent node to be scheduled
+        currentTimeStamp += waitTime;
+        timeStampMap.put(queueSize, currentTimeStamp);
+        queueSize++;
+        //Timestamp for when next node to be scheduled can start ie end timestamp of latest node
+        currentTimeStamp += node.getWeight();
+    }
+
+    public Queue<Node> getNodeQueue(){
+        return nodeQueue;
+    }
+
+    public Map<Integer,Integer> getTimeStampMap(){
+        return timeStampMap;
+    }
+
+    public int getQueueSize(){
+        return queueSize;
+    }
+
+    public int getCurrentTimeStamp() {
+        return currentTimeStamp;
+    }
 
 }
