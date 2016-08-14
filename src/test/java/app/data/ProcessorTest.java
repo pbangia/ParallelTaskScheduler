d5efed1dacc6486a5d1b8f1adc2375a77ca0ea07
taranpreet <taranpreetkohli@yahoo.co.nz>
2016-08-15 02:03:38 +1200

    Added tests for Processor class

--- src/test/java/app/data/ProcessorTest.java
+++ src/test/java/app/data/ProcessorTest.java
@@ -14,9 +14,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
-/**
- * Created by User on 9/08/2016.
- */
 public class ProcessorTest {
 
     private Processor processor;
@@ -28,26 +25,24 @@ public class ProcessorTest {
 
     @Test
     public void testProcessorInstantiation_CorrectlyInitialFields() throws AppException {
-        assertTrue(processor.getQueueSize() == 0);
         assertTrue(processor.getCurrentTimeStamp() == 0);
         assertTrue(processor.getNodeStartTimeMap().size() == 0);
         assertTrue(processor.getNodeQueue().size() == 0);
     }
 
     @Test
-    public void testAddNodeToQueue_noWaitTime_CorrectlyUpdatesFields() throws AppException {
+    public void testAddNodeToQueue_NoWaitTime_CorrectlyUpdatesFields() throws AppException {
         Node a = new Node("a", 1);
         processor.addNodeToQueue(a, 0);
 
-        assertTrue(processor.getQueueSize() == 1);
         assertTrue(processor.getCurrentTimeStamp() == 1);
-        assertTrue(processor.getNodeStartTimeMap().size() == 1);
+        assertTrue(processor.getNodeStartTimeMap().get(a) == 0);
         assertTrue(processor.getNodeQueue().size() == 1);
 
     }
 
     @Test
-    public void testAddNodeToQueue_withWaitTime_CorrectlyUpdatesTimeStampField() throws AppException {
+    public void testAddNodeToQueue_WithWaitTime_CorrectlyUpdatesTimeStampField() throws AppException {
         Node a = new Node("a", 2);
         processor.addNodeToQueue(a, 1);
 
@@ -56,48 +51,22 @@ public class ProcessorTest {
 
     @Test
     public void testClone() throws Exception {
-        Node n1a = new Node("A",1);
-        Node n1b = new Node("B",1);
-        Queue<Node> nq1 = new ConcurrentLinkedQueue<Node>();
-        nq1.add(n1a);
-        nq1.add(n1b);
-        Map<Integer, Integer> ts1=new ConcurrentHashMap<Integer, Integer>();
-        ts1.put(1,1);
-        ts1.put(2,2);
-        Map<Node, Integer> ets1=new ConcurrentHashMap<Node, Integer>();
-        ets1.put(n1a,1);
-        ets1.put(n1b,1);
-        Processor p1 = new Processor();
+        Node nodeA = new Node("A", 1);
+        Node nodeB = new Node("B", 1);
+        processor.addNodeToQueue(nodeA, 0);
+        processor.addNodeToQueue(nodeB, 3);
 
-        p1.setQueueSize(1);
-        p1.setCurrentTimeStamp(1);
-        p1.setNodeStartTimeMap(ts1);
-        p1.setNodeQueue(nq1);
-        p1.setNodeEndTimeMap(ets1);
+        Processor clone = processor.clone();
 
-        Processor p2 = p1.clone();
+        assertEquals(true, isEqualCollection(processor.getNodeQueue(), clone.getNodeQueue()));
+        assertEquals(processor.getCurrentTimeStamp(), clone.getCurrentTimeStamp());
+    }
 
-        assertNotEquals(p1,p2);
-
-        assertEquals(p1.getCurrentTimeStamp(),p2.getCurrentTimeStamp());
-
-        assertEquals(p1.getQueueSize(),p2.getQueueSize());
-
-        assertTrue(CollectionUtils.isEqualCollection(p1.getNodeQueue(),p2.getNodeQueue()));
-        assertNotEquals(p1.getNodeQueue(),p2.getNodeQueue());
-
-        //Keys+Values are equal collections. Keys (nodes) are the same objects, values are not.
-        assertTrue(CollectionUtils.isEqualCollection(p1.getNodeEndTimeMap().values(),p2.getNodeEndTimeMap().values()));
-        assertNotEquals(p1.getNodeEndTimeMap().values(),p2.getNodeEndTimeMap().values());
-        assertTrue(CollectionUtils.isEqualCollection(p1.getNodeEndTimeMap().keySet(),p2.getNodeEndTimeMap().keySet()));
-        assertEquals(p1.getNodeEndTimeMap().keySet(),p2.getNodeEndTimeMap().keySet());
-        assertEquals(p1.getNodeEndTimeMap(),p2.getNodeEndTimeMap());
-
-        //unsure. assertEquals for maps compares contents of maps according to docs, not reference.
-        assertEquals(p1.getNodeStartTimeMap(),p2.getNodeStartTimeMap());
-        assertNotEquals(p1.getNodeStartTimeMap().values(),p2.getNodeStartTimeMap().values());
-
-
+    @Test
+    public void testGetTimeStamp(){
+        Node node = new Node("A", 1);
+        processor.addNodeToQueue(node, 1);
+        assertEquals(1, processor.getTimeStamp(node));
     }
 
 
