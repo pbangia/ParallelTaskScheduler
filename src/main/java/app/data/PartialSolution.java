0fd177998b8d13d05e0607996cefa0d4f34950cb
taranpreet <taranpreetkohli@yahoo.co.nz>
2016-08-15 15:30:04 +1200

    Fixed imports

--- src/main/java/app/data/PartialSolution.java
+++ src/main/java/app/data/PartialSolution.java
@@ -3,10 +3,8 @@ package app.data;
 import app.io.Syntax;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-import java.util.HashSet;
+
+import java.util.*;
 
 public class PartialSolution {
 
