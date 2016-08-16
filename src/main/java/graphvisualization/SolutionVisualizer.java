90fc14d72d4abf3279a287f2d280f2243e468407
Priyankit Bangia <priyankit.bangia@gmail.com>
2016-08-16 16:36:14 +1200

    Added color styling to graph visuals

--- src/main/java/graphvisualization/SolutionVisualizer.java
+++ src/main/java/graphvisualization/SolutionVisualizer.java
@@ -1,9 +1,10 @@
 package graphvisualization;
 
 import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.*;
 import org.graphstream.algorithm.generator.*;
-
+import org.graphstream.ui.swingViewer.Viewer;
 
 
 public class SolutionVisualizer implements IGraphPublisher{
@@ -12,6 +13,9 @@ public class SolutionVisualizer implements IGraphPublisher{
 
     public void addRoot() {
         solutionTree.addNode("0");
+        Node n = solutionTree.getNode("0");
+        n.addAttribute("ui.style", "fill-color: #6CC50D; size: 30px;");
+        n.setAttribute("xyz", 0, 0, 0);
     }
 
     public void addNode(int parent, int child) {
@@ -20,6 +24,10 @@ public class SolutionVisualizer implements IGraphPublisher{
         String edgeName = parentName + "," + childName;
 
         solutionTree.addNode(childName);
+        solutionTree.getNode(childName).addAttribute("ui.style", "fill-color: #808080; size: 20px;");
+
+        //solutionTree.getNode(childName).addAttribute("ui.style", "fill-color: ;");
+
         solutionTree.addEdge(edgeName, parentName, childName);
     }
 
@@ -31,7 +39,8 @@ public class SolutionVisualizer implements IGraphPublisher{
 
     public void init(Graph solutionTree) {
         this.solutionTree = solutionTree;
-        solutionTree.display();
+        Viewer v = solutionTree.display();
+       // v.disableAutoLayout();
     }
 
     @Override
