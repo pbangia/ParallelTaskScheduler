779ad16b757db72e225520c284f11ffb0f58fb57
Priyankit Bangia <priyankit.bangia@gmail.com>
2016-08-17 13:22:20 +1200

    Changed color to dynamic and display edges and root nodes on top of other nodes.

--- src/main/java/graphvisualization/SolutionVisualizer.java
+++ src/main/java/graphvisualization/SolutionVisualizer.java
@@ -4,7 +4,9 @@ import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.*;
 import org.graphstream.algorithm.generator.*;
+import org.graphstream.ui.swingViewer.View;
 import org.graphstream.ui.swingViewer.Viewer;
+import org.graphstream.ui.swingViewer.ViewerPipe;
 
 
 public class SolutionVisualizer implements IGraphPublisher{
@@ -14,9 +16,9 @@ public class SolutionVisualizer implements IGraphPublisher{
     public void addRoot() {
         solutionTree.addNode("0");
         Node n = solutionTree.getNode("0");
-        n.addAttribute("ui.style", "fill-color: #6CC50D;");
+        n.addAttribute("ui.style", "fill-color: #6CC50D;fill-mode: dyn-plain; z-index:1;");
         n.addAttribute("ui.label","Root");
-        n.setAttribute("xyz", 0, 0, 0);
+        //n.setAttribute("xyz", 0, 0, 0);
     }
 
     public void addNode(int parent, int child) {
@@ -25,9 +27,12 @@ public class SolutionVisualizer implements IGraphPublisher{
         String edgeName = parentName + "," + childName;
 
         solutionTree.addNode(childName);
-        solutionTree.getNode(childName).addAttribute("ui.style", "fill-color: #808080;");
+        solutionTree.getNode(childName).addAttribute("ui.style", "fill-color: #808080;fill-mode: dyn-plain;z-index:0;");
 
         solutionTree.addEdge(edgeName, parentName, childName);
+        try{
+            //Thread.sleep(2000);
+        } catch (Exception e) {}
     }
 
     public void removeNode(int child){
@@ -39,7 +44,7 @@ public class SolutionVisualizer implements IGraphPublisher{
     public void init(Graph solutionTree) {
         this.solutionTree = solutionTree;
         Viewer v = solutionTree.display();
-       // v.disableAutoLayout();
+
     }
 
     @Override
