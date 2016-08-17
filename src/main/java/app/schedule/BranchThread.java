be48c61f43ee008796097ae23ac8c65d3b5fc0fa
hailun wang <hailun wang>
2016-08-18 08:55:27 +1200

    Added TODO and comments explaining how methods worked in BranchThread

--- src/main/java/app/schedule/BranchThread.java
+++ src/main/java/app/schedule/BranchThread.java
@@ -18,11 +18,13 @@ public class BranchThread extends Thread {
     private PartialSolution currentPartialSolution;
     private TaskScheduler taskScheduler;
 
+    //TODO Branchthread should not take in a TaskScheduler instance. Change to interface reference.
     public BranchThread(TaskScheduler taskScheduler, PartialSolution currentPartialSolution){
         this.taskScheduler = taskScheduler;
         this.currentPartialSolution = currentPartialSolution;
     }
 
+    //This can now be removed I think, or change to take in interface reference
     public BranchThread(TaskScheduler taskScheduler) {
         this.taskScheduler = taskScheduler;
     }
@@ -31,6 +33,7 @@ public class BranchThread extends Thread {
         this.currentPartialSolution = currentPartialSolution;
     }
 
+    //Each thread has these tasks to executes
     @Override
     public void run(){
 
@@ -43,12 +46,15 @@ public class BranchThread extends Thread {
             return;
         }
 
+        //Reached end of solution tree, instead of comparing if this is the best partial solution, it gives the
+        //current solution it just computed to taskScheduler which will then conpare
         if (nextAvailableNodes.isEmpty()) {
             taskScheduler.setBestPartialSolution(currentPartialSolution);
             this.notifyAll();
             return;
         }
 
+        //After creation of all new partial solutions where node can be places next, gives this to taskScheduler to add onto the stack
         for (Node availableNode : nextAvailableNodes) {
             List<PartialSolution> availablePartialSolutions = SchedulerHelper.getAvailablePartialSolutions(availableNode, currentPartialSolution, currentPartialSolution.getNumberOfProcessors());
             taskScheduler.addPartialSolutions(availablePartialSolutions);
