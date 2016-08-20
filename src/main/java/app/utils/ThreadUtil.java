1dcdc44a2720f81c248fe9428acbad765c12ce02
Taranpreet Kohli <tkoh638@aucklanduni.ac.nz>
2016-08-20 16:40:02 +1200

    Changed ThreadUtil to static

--- src/main/java/app/utils/ThreadUtil.java
+++ src/main/java/app/utils/ThreadUtil.java
@@ -4,22 +4,21 @@ package app.utils;
 import java.util.List;
 
 public class ThreadUtil {
-    public int incrementIndex(int currentIndex, int size){
-        if(currentIndex == size-1){
+
+    public static int incrementIndex(int currentIndex, int size) {
+        if (currentIndex == size - 1) {
             return 0;
         } else {
             return currentIndex++;
         }
     }
 
-    public boolean allInactive(List<? extends Thread> branchThreadList) {
-        for (Thread thread : branchThreadList){
-            if (thread.isAlive()){
+    public static boolean allInactive(List<? extends Thread> branchThreadList) {
+        for (Thread thread : branchThreadList) {
+            if (thread.isAlive()) {
                 return false;
             }
         }
-
         return true;
-
     }
 }
