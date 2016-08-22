7d6d64b387ec2129caa9a56f114552e3e732c195
hailun wang <hailun wang>
2016-08-23 11:34:03 +1200

    Fixed factory to return correct CommonScheduler

--- src/main/java/app/schedule/TaskSchedulerFactory.java
+++ src/main/java/app/schedule/TaskSchedulerFactory.java
@@ -8,16 +8,18 @@ import java.util.Map;
 
 public class TaskSchedulerFactory {
 
-    public static ParallelScheduler createTaskScheduler(Map<String, Node> dataMap, int numProcessors, int numThreads) {
+    public static CommonScheduler createTaskScheduler(Map<String, Node> dataMap, int numProcessors, int numThreads) {
 
         if (numThreads == 0) {
-            return new SerialScheduler(dataMap.values(), numProcessors)
-        }
-        List<BranchThread> branchThreads = new ArrayList<>(numThreads);
-        for (int i = 0; i < numThreads; i++){
-            branchThreads.add(i, new BranchThread());
+            return new SerialScheduler(dataMap.values(), numProcessors);
+        } else {
+            List<BranchThread> branchThreads = new ArrayList<>(numThreads);
+            for (int i = 0; i < numThreads; i++){
+                branchThreads.add(i, new BranchThread());
+            }
+
+            return new ParallelScheduler(dataMap.values(), numProcessors, branchThreads);
         }
 
-        return new ParallelScheduler(dataMap.values(), numProcessors, branchThreads);
     }
 }
